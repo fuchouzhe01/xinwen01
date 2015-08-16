f015c5888d053e21f04270302b70199efc25841a
xiaomajiao <297182858@qq.com>
2015-08-16 09:28:43 +0800

    新项目

---
+++ xinwen01/xinwen01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xinwen01
+//
+//  Created by 罗凤翔 on 15/8/16.
+//  Copyright (c) 2015年 lfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xinwen01/xinwen01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinwen01
+//
+//  Created by 罗凤翔 on 15/8/16.
+//  Copyright (c) 2015年 lfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
