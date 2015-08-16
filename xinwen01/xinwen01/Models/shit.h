aad4e347309553b766e6f8d110e7c170ffa2302d
xiaomajiao <297182858@qq.com>
2015-08-16 10:09:53 +0800

    添加models文件夹

---
+++ xinwen01/xinwen01/Models/shit.h
@@ -0,0 +1,13 @@
+//
+//  shit.h
+//  xinwen01
+//
+//  Created by 罗凤翔 on 15/8/16.
+//  Copyright (c) 2015年 lfx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface shit : NSObject
+
+@end
---
+++ xinwen01/xinwen01/Models/shit.m
@@ -0,0 +1,13 @@
+//
+//  shit.m
+//  xinwen01
+//
+//  Created by 罗凤翔 on 15/8/16.
+//  Copyright (c) 2015年 lfx. All rights reserved.
+//
+
+#import "shit.h"
+
+@implementation shit
+
+@end
