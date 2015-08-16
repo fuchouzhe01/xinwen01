1b5d8a643daeff6fb3564c7d26abda22a284fdc5
xiaomajiao <297182858@qq.com>
2015-08-16 10:12:06 +0800

    添加views 文件夹

--- xinwen01/xinwen01/Views/ssd.m
+++ xinwen01/xinwen01/Views/ssd.m
@@ -1,16 +1,13 @@
 //
-//  shit.m
+//  ssd.m
 //  xinwen01
 //
 //  Created by 罗凤翔 on 15/8/16.
 //  Copyright (c) 2015年 lfx. All rights reserved.
 //
 
-#import "shit.h"
+#import "ssd.h"
 
-@implementation shit
-
-//有人说它是一坨屎
-
+@implementation ssd
 
 @end
