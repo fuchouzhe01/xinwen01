235af60ddf935dfa11be302bb50f0e052308cc81
nansong <nansong@nansongdeMacBook-Pro.local>
2015-08-16 09:48:32 +0800

    测试push

--- xinwen01/xinwen01/ViewController.m
+++ xinwen01/xinwen01/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //我这里也要改
     self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
